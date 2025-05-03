ddfd2d0ef86f10943d13244fc40d343b88ad60ff
radha-rk <rrkk465789@gmail.com>
2025-05-03 14:04:25 +0530

    Add User model schema with passport-local-mongoose plugin

--- pinterest/routes/users.js
+++ pinterest/routes/users.js
@@ -1,9 +1,38 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 var express = require('express');
 var router = express.Router();
 
+mongoose.connect("mongodb://127.0.0.1:27017/nayaappPinterest");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  dp: String, // Display picture URL
+  post: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Pin' // Assuming your post/pin model is named 'Pin'
+    }
+  ]
+})
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-module.exports = router;
+userSchema.plugin(plm);
+module.exports = mongoose.model('user',userSchema);
